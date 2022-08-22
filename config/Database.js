ade47a7e2563fa16f7e68cfd109be70434c642bc
Ubuntu <ubuntu@ip-10-0-0-132.ap-southeast-1.compute.internal>
2022-08-22 12:34:11 +0000

    second commit

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,8 @@
 import {Sequelize} from "sequelize";
+import 'dotenv/config';
 
-const db = new Sequelize('people','people','people',{
-    host: 'localhost',
+const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 });
 
